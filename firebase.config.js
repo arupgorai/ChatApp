52a8f746b23b6e5fe703be25f03c0aba8c635ffe
Arup Gorai 😎 <arup.gorai@digital.datamatics.com>
2022-07-10 22:12:54 +0530

    firebase setup done

---
+++ firebase.config.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { initializeFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0scTAwgSuyFFHWAnZAizlJYucRGe1tWE",
+  authDomain: "chatapp-eea23.firebaseapp.com",
+  projectId: "chatapp-eea23",
+  storageBucket: "chatapp-eea23.appspot.com",
+  messagingSenderId: "14540545804",
+  appId: "1:14540545804:web:7319a8c1927b69b52c7329",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+
+export function signIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+export function signUp(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
